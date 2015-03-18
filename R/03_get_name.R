c7591b9e83e6de400fb123e67c37030ad6a7953d
your_username <your_email@example.com>
2015-03-18 16:03:03 -0400

    Updated get_name to deal with multiple formats

--- R/03_get_name.R
+++ R/03_get_name.R
@@ -11,28 +11,48 @@
 #' [[1]]
 #' [1] "First name" "Last name" 
 
-get_name <- function(student_rcard){
-  ## Load libraries
-  library(stringr)
+get_name <- function(student_rcard, format_type){
   
   ## Grab student's name
+  name_separator <- list(format1 = "Student Name:", format2 = "Parent or guardian of")
   # detect student's name element
-  name_line <- str_detect(student_rcard, "Student Name:")
-  # Extract element containing student's name
-  student_rcard <- student_rcard[name_line]
-  # Remove potential duplicates
-  student_rcard <- student_rcard[1]
+  name_line <- stringr::str_detect(student_rcard, name_separator[[format_type]])
   
-  
-  # Extract first name
-  # matches everything after the comma & space, and before the first space
-  first_name <- str_extract(student_rcard, perl("(, )[A-Z][a-z]+()"))
-  # matches only letters in the string
-  first_name <- str_extract(first_name, perl("[A-Z][a-z]+"))
-  
-  # Extract last name
-  # matches everything before the first comma and the first space before the comma.
-  last_name <- str_extract(student_rcard, perl("\\S+(?=\\,)"))
+  if (format_type == 'format1') {
+    # Extract element containing student's name
+    student_rcard <- student_rcard[name_line]
+    # Remove potential duplicates
+    student_rcard <- student_rcard[1]
+    
+    
+    # Extract first name
+    # matches everything after the comma & space, and before the first space
+    first_name <- stringr::str_extract(student_rcard, perl("(, )[A-Z][a-z]+()"))
+    # matches only letters in the string
+    first_name <- stringr::str_extract(first_name, perl("[A-Z][a-z]+"))
+    
+    # Extract last name
+    # matches everything before the first comma and the first space before the comma.
+    last_name <- stringr::str_extract(student_rcard, perl("\\S+(?=\\,)"))
+    
+  } else {
+    # Extract element containing student's name
+    student_rcard <- student_rcard[name_line]
+    # Remove potential duplicates
+    student_rcard <- student_rcard[1]
+    
+    
+    # Extract first name
+    # matches everything after the comma & space, and before the first space
+    first_name <- stringr::str_extract(student_rcard, perl("(, )[A-Z][a-z]+()"))
+    # matches only letters in the string
+    first_name <- stringr::str_extract(first_name, perl("[A-Z][a-z]+"))
+    
+    # Extract last name
+    # matches everything before the first comma and the first space before the comma.
+    last_name <- stringr::str_extract(student_rcard, perl("\\S+(?=\\,)"))
+    
+  }
   
   ## Return student's name
   return(list(c(fname = first_name, lname = last_name)))
