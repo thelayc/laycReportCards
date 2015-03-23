cd3e08058615332a97e7b37fea4572d1d9273741
your_username <your_email@example.com>
2015-03-23 17:58:20 -0400

    finalized draft version of function (works)

--- R/11_get_grades_f2.R
+++ R/11_get_grades_f2.R
@@ -19,64 +19,31 @@ get_grades_f2 <- function(student_rcard){
   chunk_end <- which(stringr::str_detect(student_rcard, 'Teacher comment') == TRUE)
   # Extract chunk
   chunk <- student_rcard[chunk_start:chunk_end]
+  chunk <- chunk[!is.na(chunk)]
+  # Extract grade lines
+  grade_lines <- chunk[3:length(chunk)-1]
   
   #2) Identify position of each grade on the line
-  # Use "Term", as a position markers
+  # Use "TERM", as a position markers
   # Extract element containing TERM"
   term <- student_rcard[chunk_start]
+  term <- stringr::str_locate_all(term, 'TERM')
   # Locate the starting position of "Term 1" in the string - First occurrence of TERM
-  g1_position <- stringr::str_locate(term, 'Term 1')[1]
+  g1_position <- term[[1]][1, ]['start']
   # Locate the starting position of "Term 2" in the string
-  g2_position <- stringr::str_locate(term, 'Term 2')[1]
+  g2_position <- term[[1]][2, ]['start']
   # Locate the starting position of "Term 3" in the string
-  g3_position <- stringr::str_locate(term, 'Term 3')[1]
+  g3_position <- term[[1]][3, ]['start']
   # Locate the starting position of "Term 4" in the string
-  g4_position <- stringr::str_locate(term, 'Term 4')[1]
+  g4_position <- term[[1]][4, ]['start']
   # Locate the starting position of "Exam" in the string
-  exam_position <- stringr::str_locate(term, 'Exam')[1]
+  #exam_position <- stringr::str_locate(term, 'Exam')[1]
   # Locate the starting position of "Term 4" in the string
-  final_position <- stringr::str_locate(term, 'Final')[1]
+  #final_position <- stringr::str_locate(term, 'Final')[1]
   
-  ###############################################
-  #1) Identify grades alignment on the page
-  # Detect line that starts with "Course"
-  course_line <- stringr::str_detect(student_rcard, '^Course')
-  # Extract lines starting with "Course"
-  course <- student_rcard[course_line]
-  # Remove potential duplicates
-  course <- course[1]
-  # Locate the starting position of "Sec" in the string
-  sec_position <- stringr::str_locate(course, 'Sec')[1]
-  
-  #2) Detect lines with grades information
-  grade_lines <- stringr::str_detect(substr(student_rcard, sec_position, sec_position + 2), '^[0-9]{2}')
-  # Extract grade lines
-  grade_lines <- student_rcard[grade_lines]
-  
-  #3) Identify position of each grade on the line
-   # Use "Term 1", "Term 2", ..., "Exam", and "Final" as position markers
-  # detect where grades information begins
-  term_line <- stringr::str_detect(student_rcard, "Term 1")
-  # Extract element containing Term 1"
-  term <- student_rcard[term_line]
-  # Remove potential duplicates
-  term <- term[1]
-  # Locate the starting position of "Term 1" in the string
-  g1_position <- stringr::str_locate(term, 'Term 1')[1]
-  # Locate the starting position of "Term 2" in the string
-  g2_position <- stringr::str_locate(term, 'Term 2')[1]
-  # Locate the starting position of "Term 3" in the string
-  g3_position <- stringr::str_locate(term, 'Term 3')[1]
-  # Locate the starting position of "Term 4" in the string
-  g4_position <- stringr::str_locate(term, 'Term 4')[1]
-  # Locate the starting position of "Exam" in the string
-  exam_position <- stringr::str_locate(term, 'Exam')[1]
-  # Locate the starting position of "Term 4" in the string
-  final_position <- stringr::str_locate(term, 'Final')[1]
-  
-  #4) Grab grades based on the position information
+  #3) Grab grades based on the position information
   # grab course names
-  courses <- stringr::str_trim(substr(grade_lines, sec_position + 5, g1_position - 1))
+  courses <- stringr::str_trim(substr(grade_lines, 1, g1_position - 1))
   # grab grades from term 1
   term1 <- stringr::str_trim(substr(grade_lines, g1_position, g2_position - 1))
   # grab grades from term 2
@@ -84,16 +51,17 @@ get_grades_f2 <- function(student_rcard){
   # grab grades from term 3
   term3 <- stringr::str_trim(substr(grade_lines, g3_position, g4_position - 1))
   # grab grades from term 4
-  term4 <- stringr::str_trim(substr(grade_lines, g4_position, exam_position - 1))
+  term4 <- stringr::str_trim(substr(grade_lines, g4_position, g4_position + 5))
   # grab grades from exam
-  exam <- stringr::str_trim(substr(grade_lines, exam_position, final_position - 1))
+  #exam <- stringr::str_trim(substr(grade_lines, exam_position, final_position - 1))
   # grab grades from final
-  final <- stringr::str_trim(substr(grade_lines, final_position, final_position + 10))
+  #final <- stringr::str_trim(substr(grade_lines, final_position, final_position + 10))
   
   # Create a dataframe
-  grades <- data.frame(courses = courses, term1 = term1, term2 = term2, term3 = term3, term4 = term4, exam = exam, final = final)
+  grades <- data.frame(courses = courses, term1 = term1, term2 = term2, term3 = term3, term4 = term4)
   # Remove extra rows
-  grades <- grades[grades$courses != 'Authorized PM Off Campus', ]
+  grades <- grades[grades$courses != '', ]
+  grades <- grades[grades$courses != 'EDUCATION', ]
   # Replace blank by NAs: TO BE DONE
   
   # Melt data frame
--- TEMP.R
+++ TEMP.R
@@ -1,14 +1,18 @@
+library(dplyr)
+library(plyr)
+library(tidyr)
+library(stringr)
 # Testing code for FORMAT1 ------------------------------------------------
 
 list.files('../temp_data', full.names = TRUE, recursive = TRUE)
 format_type <- 'format1'
 txt <- read_pdf("../temp_data/report_cards/LAYC-Wilson report cards.pdf")
 rcards <- split_students(txt, format_type)
-student_rcard <- rcards[[1]]
+student_rcard <- rcards[[334]]
 # Testing get_names
 get_name(student_rcard, format_type)
 names <- lapply(rcards, get_name, format_type)
-names[[1]]
+names[[334]]
 
 debug(get_grades)
 get_grades(student_rcard)
@@ -22,9 +26,82 @@ txt <- read_pdf('../temp_data/report_cards/Powell ES Term2 report cards SY14-15.
 rcards <- split_students(txt, format_type = 'format2')
 student_rcard <- rcards[[1]]
 # Testing get_names
-get_name(student_rcard, format_type = 'format2')
+#get_name(student_rcard, format_type = 'format2')
 names <- lapply(rcards, get_name, format_type = 'format2')
-names[[1]]
+names <- data.frame(sapply(names,c), stringsAsFactors = FALSE)
+names <- data.frame(t(names))
+#names[[1]]
+# Testing get_grades_f2
+debug(get_grades_f2)
+undebug(get_grades_f2)
+get_grades_f2(student_rcard)
+## Create a dataframe of topic grades
+topics <- lapply(rcards, get_grades_f2)
+topics <- do.call("rbind.fill", topics)
+topics['value'] <- NULL
+
+## Bind dataframes
+powell <- cbind(names, topics)
+powell[] <- lapply(powell, as.character)
+my_col <- colnames(powell)
+my_col <- str_replace_all(my_col, ' ', '.')
+colnames(powell) <- my_col
+powell %>%
+  gather(topic, value, ART_term1:WORLD.LANGUAGE_term4) %>%
+  separate(topic, into = c('topic', 'term'), sep= '_') ->
+  powell2
+
+powell2 <- powell2[powell2$value != '', ]
+powell2 <- powell2[!is.na(powell2$value), ]
+powell2$topic <- tolower(powell2$topic)
+powell2$value <- as.numeric(powell2$value)
+
+
+
+powell2 %>%
+  spread(term, value) %>%
+  mutate(change = term2 - term1) ->
+  powell2
+
+powell2$topic <- str_replace_all(powell2$topic, '\\.', '_')
+
+powell2$change_ord[powell2$change > 0] <- 'positive'
+powell2$change_ord[powell2$change == 0] <- 'no change'
+powell2$change_ord[powell2$change < 0] <- 'negative'
+
+powell2$custom_id <- paste0(powell2$fname, powell2$lname)
+powell2$custom_id <- tolower(powell2$custom_id)
+
+
+# match with ETO participants
+
+eto <- laycUtils::load_txt('../temp_data/powell_eto.txt')
+eto <- laycUtils::clean_data(eto)
+eto$custom_id <- paste0(eto$fname, eto$lname)
+
+eto %>%
+  separate(custom_id, into = c('custom_id', 'trash'), by = '-', extra = 'drop') %>%
+  select(-trash) %>%
+  left_join(powell2, by = 'custom_id') ->
+  powell3
+
+length(unique(powell3$id))
+
+write.csv(powell3, file = '../temp_data/powell.csv', row.names = FALSE, na = '')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
