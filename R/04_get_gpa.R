aaba0f073fd30e02adfc26b323f68e22c46c63c2
tonyfujs <tonyfujs@gmail.com>
2015-05-22 12:03:41 -0400

    added checks to get_gpa

--- R/04_get_gpa.R
+++ R/04_get_gpa.R
@@ -10,42 +10,51 @@
 #' get_gpa(students_list[[1]])
 
 get_gpa <- function(student_rcard){
-  ## Load libraries
-  library(stringr)
   
-  ## Grab student's gpas
+  # 1 - Extract lines containing gpa information
   # detect gpa element
-  gpa_line <- str_detect(student_rcard, "Grade Point Average")
+  gpa_line <- grepl(x = student_rcard, pattern = "Grade Point Average")
+  # CHECK that pattern has been matched
+  assertthat::assert_that(sum(gpa_line) > 0)
   # Extract element containing gpa
   student_rcard <- student_rcard[gpa_line]
   # Remove potential duplicates
   student_rcard <- student_rcard[1] 
   # Remove blank spaces
-  student_rcard <- str_replace_all(student_rcard, " ", "")
+  student_rcard <- gsub(x = student_rcard, pattern = " ", replacement = "")
   
-  ## Extract gpa first quarter
+  # 2- Extract gpa scores
+  # first quarter
+  # CHECK: If there is a match for first quarter
+  assertthat::assert_that(grepl(pattern = '1=', x = student_rcard))
   # matches number with format 0.00 after "1="
-  gpa1 <- str_extract(student_rcard, perl("1=[0-9]\\.[0-9]{2}")) 
+  gpa1 <- stringr::str_extract(student_rcard, stringr::regex("1=[0-9]\\.[0-9]{2}")) 
   # Remove "1="
-  gpa1 <- str_replace(gpa1, "1=", "") 
+  gpa1 <- stringr::str_replace(gpa1, "1=", "") 
   
-  # Extract gpa second quarter
+  # second quarter
+  # CHECK: If there is a match for second quarter
+  assertthat::assert_that(grepl(pattern = '2=', x = student_rcard))
   # matches number with format 0.00 after "2="
-  gpa2 <- str_extract(student_rcard, perl("2=[0-9]\\.[0-9]{2}")) 
+  gpa2 <- stringr::str_extract(student_rcard, stringr::regex("2=[0-9]\\.[0-9]{2}")) 
   # Remove "2="
-  gpa2 <- str_replace(gpa2, "2=", "") 
+  gpa2 <- stringr::str_replace(gpa2, "2=", "") 
   
-  # Extract gpa third quarter
+  # third quarter
+  # CHECK: If there is a match for third quarter
+  assertthat::assert_that(grepl(pattern = '3=', x = student_rcard))
   # matches number with format 0.00 after "3="
-  gpa3 <- str_extract(student_rcard, perl("3=[0-9]\\.[0-9]{2}"))
+  gpa3 <- stringr::str_extract(student_rcard, stringr::regex("3=[0-9]\\.[0-9]{2}"))
   # Remove "2="
-  gpa3 <- str_replace(gpa3, "3=", "") 
+  gpa3 <- stringr::str_replace(gpa3, "3=", "") 
   
-  # Extract gpa fourth quarter
+  # fourth quarter
+  # CHECK: If there is a match for fourth quarter
+  assertthat::assert_that(grepl(pattern = '4=', x = student_rcard))
   # matches number with format 0.00 after "4="
-  gpa4 <- str_extract(student_rcard, perl("4=[0-9]\\.[0-9]{2}")) 
+  gpa4 <- stringr::str_extract(student_rcard, stringr::regex("4=[0-9]\\.[0-9]{2}")) 
   # Remove "2="
-  gpa4 <- str_replace(gpa4, "4=", "") 
+  gpa4 <- stringr::str_replace(gpa4, "4=", "") 
   
   ## Return student's gpa
   return(list(c(gpa1 = gpa1, gpa2 = gpa2, gpa3 = gpa3, gpa4 = gpa4)))
