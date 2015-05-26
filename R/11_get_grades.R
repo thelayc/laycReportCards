ea738ac2111a6557fc8134775c52b1fd188c7b4b
your_username <your_email@example.com>
2015-05-26 14:44:01 -0400

    modified get_grades

--- R/11_get_grades.R
+++ R/11_get_grades.R
@@ -24,10 +24,12 @@ get_grades <- function(student_rcard){
   # Locate the starting position of "Sec" in the string
   sec_position <- stringr::str_locate(course, 'Sec')[1]
   
-  #2 - Detect lines with grades information
+  #2 - Extract lines with grades information
   grade_lines <- stringr::str_detect(substr(student_rcard, sec_position, sec_position + 2), '^[0-9]{2}')
   # Extract grade lines
   grade_lines <- student_rcard[grade_lines]
+  # CHECK that lines exist
+  assertthat::assert_that(length(grade_lines) > 0)
   
   #3 - Identify position of each grade on the line
    # Use "Term 1", "Term 2", ..., "Exam", and "Final" as position markers
@@ -37,6 +39,7 @@ get_grades <- function(student_rcard){
   term <- student_rcard[term_line]
   # Remove potential duplicates
   term <- term[1]
+  
   # Locate the starting position of "Term 1" in the string
   g1_position <- stringr::str_locate(term, 'Term 1')[1]
   # Locate the starting position of "Term 2" in the string
@@ -50,7 +53,7 @@ get_grades <- function(student_rcard){
   # Locate the starting position of "Term 4" in the string
   final_position <- stringr::str_locate(term, 'Final')[1]
   
-  #4) Grab grades based on the position information
+  # 4 - Grab grades based on the position information
   # grab course names
   courses <- stringr::str_trim(substr(grade_lines, sec_position + 5, g1_position - 1))
   # grab grades from term 1
@@ -65,24 +68,23 @@ get_grades <- function(student_rcard){
   exam <- stringr::str_trim(substr(grade_lines, exam_position, final_position - 1))
   # grab grades from final
   final <- stringr::str_trim(substr(grade_lines, final_position, final_position + 10))
+  # TO DO: Insert check that all vectors have the same length
   
-  # Create a dataframe
-  grades <- data.frame(courses = courses, term1 = term1, term2 = term2, term3 = term3, term4 = term4, exam = exam, final = final)
+  # 5 - Create a dataframe
+  grades <- data.frame(courses = courses, term1 = term1, term2 = term2, term3 = term3, term4 = term4, exam = exam, final = final, stringsAsFactors = FALSE)
   # Remove extra rows
   keep <- !grepl(pattern = '^Authorized', x = grades$courses)
   grades <- grades[keep, ]
-# <<<<<<< HEAD
-#   # Remove potential duplicated rows
-#   dup <- duplicated(grades$courses)
-#   grades <- grades[!dup, ]
-# =======
-# >>>>>>> format2
-  # Replace blank by NAs: TO BE DONE
+
+  # Remove potential duplicated rows
+  dup <- duplicated(grades$courses)
+  grades <- grades[!dup, ]
+  # Replace blank by NAs
+  grades[] <- lapply(grades, function(x) ifelse(x == "", NA, x))
   
+  # 6 - reshape data frame
   # Melt data frame
   grades <- reshape2::melt(grades, id = c('courses'))
-  # Remove rows with empty value
-  grades$value <- stringr::str_replace_all(grades$value, " ", "")
   # Make wide
   grades <- reshape::cast(grades, ~ courses + variable)
   
